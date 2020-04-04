1d9908acbbaff4bafa8aa35e98cdeafc04a7d24d
IvanSiniy <63054072+IvanSiniy@users.noreply.github.com>
2020-04-04 17:15:54 +0300

    Обновлен файл до версии 0.1 НЕИГРАБЕЛЬНО

--- piska.pas
+++ piska.pas
@@ -2,5 +2,10 @@ Program tt;
 var
 s:string;
 begin
-writeln('¬Œ“ œ»—‹ ¿: .!.');
+writeln('FUCK: .!.');
+writeln('Welcome to the CMD');
+writeln('Write 1 if you CAMEN');
+writeln('Write 2 if you BUMAGA');
+writeln('Write 3 if you NOZHNIZI');
+
 end.
