f877a8b5945c7b946b4aa84c4ef9b3729c067195
IvanSiniy <63054072+IvanSiniy@users.noreply.github.com>
2020-04-04 17:37:58 +0300

    RABOTAET BLYAT

--- piska.pas
+++ piska.pas
@@ -1,14 +1,21 @@
 Program tt; 
 var
-s,i:string;
+r,i:integer;
+s,win,lose,draw:string;
 begin
 writeln('FUCK: .!.');
 writeln('Welcome to the CMD');
-writeln('Write 1 if you CAMEN');
-writeln('Write 2 if you BUMAGA');
-writeln('Write 3 if you NOZHNIZI');
+writeln('Write 1 if Camen');
+writeln('Write 2 if Bumaga');
+writeln('Write 3 if Nozhnizi');
+lose:='YOU LOSE. GIVE ME MONEY';
+win:='YOU WIN, BUT GIVE ME MONEY';
+draw:='DRAW';
 readln(i);
-if i=1 then writeln('YOU LOSE, GIVE ME MONEY');
-if i=2 then writeln('YOU LOSE, GIVE ME MONEY');
-if i=3 then writeln('YOU LOSE, GIVE ME MONEY');
+r:=random(3)+1;
+writeln('Bot vibral:',r);
+if i=r then writeln(draw) else
+if (i=3) and (r=2)  then writeln(win) else
+if (i=1) and (r=3) then writeln(win) else
+if (i=2) and (r=1)  then writeln(win) else writeln(lose);
 end.
