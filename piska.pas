8a52621197b581cd21a782e0e69a848029e8e26d
IvanSiniy <63054072+IvanSiniy@users.noreply.github.com>
2020-04-04 17:17:56 +0300

    Обновлен до версии Идеальнео казино 2.0 (Всегда луз)

--- piska.pas
+++ piska.pas
@@ -1,11 +1,14 @@
 Program tt; 
 var
-s:string;
+s,i:string;
 begin
 writeln('FUCK: .!.');
 writeln('Welcome to the CMD');
 writeln('Write 1 if you CAMEN');
 writeln('Write 2 if you BUMAGA');
 writeln('Write 3 if you NOZHNIZI');
-
+readln(i);
+if i=1 then writeln('YOU LOSE, GIVE ME MONEY');
+if i=2 then writeln('YOU LOSE, GIVE ME MONEY');
+if i=3 then writeln('YOU LOSE, GIVE ME MONEY');
 end.
